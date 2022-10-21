1c58d81667c952c533ba461208bab4f41c4dbd2a
Walid-Abbes <walid.abbes@etu.usthb.dz>
2022-10-21 18:16:59 +0100

    m

---
+++ apps/store/data.js
@@ -0,0 +1,98 @@
+let itemdata = [
+  {
+    id: "a",
+    name: "Book 1",
+    price: "1500",
+    description: " book book book book book book book book book book book book book book book book",
+    img: "/last-project/assets/books/book-1.png",
+  },
+  
+  {
+    id: "b",
+    name: "Book 2",
+    price: "1500",
+    description: " book book book book book book book book book book book book book book book book",
+    img: "/last-project/assets/books/book-2.png",
+  },
+  
+  {
+    id: "c",
+    name: "Book 3",
+    price: "1500",
+    description: " book book book book book book book book book book book book book book book book",
+    img: "/last-project/assets/books/book-3.png",
+  },
+  
+  {
+    id: "d",
+    name: "Book 4",
+    price: "1500",
+    description: " book book book book book book book book book book book book book book book book",
+    img: "/last-project/assets/books/book-4.png",
+  },
+  
+  {
+    id: "e",
+    name: "Book 5",
+    price: "1500",
+    description: " book book book book book book book book book book book book book book book book",
+    img: "/last-project/assets/books/book-5.png",
+  },
+  
+  {
+    id: "f",
+    name: "Book 6",
+    price: "1500",
+    description: " book book book book book book book book book book book book book book book book",
+    img: "/last-project/assets/books/book-6.png",
+  },
+  
+  {
+    id: "g",
+    name: "Book 7",
+    price: "1500",
+    description: " book book book book book book book book book book book book book book book book",
+    img: "/last-project/assets/books/book-7.png",
+  },
+  
+  {
+    id: "i",
+    name: "Book 8",
+    price: "1500",
+    description: " book book book book book book book book book book book book book book book book",
+    img: "/last-project/assets/books/book-8.png",
+  },
+  
+  {
+    id: "j",
+    name: "Book 9",
+    price: "1500",
+    description: " book book book book book book book book book book book book book book book book",
+    img: "/last-project/assets/books/book-9.png",
+  },
+  
+  {
+    id: "k",
+    name: "Book 10",
+    price: "1500",
+    description: " book book book book book book book book book book book book book book book book",
+    img: "/last-project/assets/books/book-10.png",
+  },
+  
+  {
+    id: "l",
+    name: "Book 11",
+    price: "1500",
+    description: " book book book book book book book book book book book book book book book book",
+    img: "/last-project/assets/books/book-11.png",
+  },
+  
+  {
+    id: "o",
+    name: "Book 12",
+    price: "1500",
+    description: " book book book book book book book book book book book book book book book book",
+    img: "/last-project/assets/books/book-12.png",
+  }
+  
+];
